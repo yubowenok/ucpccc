b5f2df9973bbbcadcb9410f190ff90bcf6d43c7d
John Zhang <johnrzhang@gmail.com>
2017-03-09 00:30:05 -0500

    Change date options to go up to 2020

--- judge/jconfig.php
+++ judge/jconfig.php
@@ -188,7 +188,7 @@ END;
          }
          print "         </select>\n";
          print "         <select name=\"cdyear\">\n";
-         for ($i = 2010; $i <= 2013; $i++) {
+         for ($i = 2017; $i <= 2020; $i++) {
             $sel = ($i == $cdyear) ? "selected=\"selected\"" : "";         
             print "            <option value=\"$i\" $sel>$i</option>\n";
          }
